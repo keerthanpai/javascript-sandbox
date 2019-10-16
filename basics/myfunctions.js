93be5f4bb28c7c60d6b8ba3b2bfecf53c98bf145
keerk121 <keerk121@in.ibm.com>
2019-10-16 13:49:57 +0530

    Import export functions

--- basics/myfunctions.js
+++ basics/myfunctions.js
@@ -1,9 +1,15 @@
 add = (a,b) => a+b;
 mul = (a,b) => a*b;
 
-module.exports.addfun = add;
-module.exports.mulfun = mul;
+//module.exports.addfun = add;
+//module.exports.mulfun = mul;
 
+/*
 module.exports = {
     addx:add,mulx:mul
+}
+*/
+
+module.exports = {
+    add,mul
 }
--- basics/usemyfunctions.js
+++ basics/usemyfunctions.js
@@ -1,8 +1,11 @@
 //const add = require('./myfunctions').addfun; //No need to import js file
 //const mul = require('./myfunctions').mulfun; 
 
-const add = require('./myfunctions').addx; //No need to import js file
-const mul = require('./myfunctions').mulx; 
+//const add = require('./myfunctions').addx; //No need to import js file
+//const mul = require('./myfunctions').mulx; 
+
+const add = require('./myfunctions').add;
+const mul = require('./myfunctions').mul;
 
 console.log(add(10,20));
 console.log(mul(10,20));
