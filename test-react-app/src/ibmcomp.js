eda1dfa0dacb7de966a90e6d3c70087578eb3406
keerk121 <keerk121@in.ibm.com>
2019-10-17 11:01:49 +0530

    Extending React Component

---
+++ test-react-app/src/ibmcomp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class IBMComp extends React.Component {
+    render(){
+        return (
+            <div>
+                <h3> This is IBM Component </h3>
+            </div>
+        )   
+    }
+}
