ace2d476b9641f5c5590ff08b82f54b5508bf867
keerk121 <keerk121@in.ibm.com>
2019-10-18 15:19:33 +0530

    State without using redux. Each component maintains its state

--- redux-app/README.md
+++ redux-app/README.md
@@ -1,3 +1,8 @@
+##Custom actions performed on this app
+
+###npm install --save redux
+###npm install --save react-redux
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
---
+++ redux-app/src/counter/index.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react'
+
+export function Counter(){
+    const [counter, setCounter] = useState(0);  //https://reactjs.org/docs/hooks-state.html
+
+    let increment = () => {
+        setCounter(counter+1);
+    }
+
+    let decrement = () => {
+        setCounter(counter-1);
+    }
+
+
+    return(
+        <div className="container">
+            <h2>Counter : {counter}</h2>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+        </div>
+    )
+}
