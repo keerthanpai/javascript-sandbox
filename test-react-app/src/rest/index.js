ad221442119d6750020e4e074dd36883ed6b4753
keerk121 <keerk121@in.ibm.com>
2019-10-18 11:22:56 +0530

    Hooks to set state for function

--- test-react-app/src/rest/index.js
+++ test-react-app/src/rest/index.js
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
-let getProducsts = () => {
-    let url = "https://jsonplaceholder.typicode.com/users";
-    axios.get(url).then( (res) => {
-        alert(JSON.stringify(res.data));
-
-    }).catch((err) => {
-        alert("error getting products")
-    });
-}
-
 export function RestComp(){
+    const [users, setUsers] = useState([]);
+
+    let getProducts = () => {
+        let url = "https://jsonplaceholder.typicode.com/users";
+        axios.get(url).then( (res) => {
+            //alert(JSON.stringify(res.data));
+            setUsers([...res.data])
+        }).catch((err) => {
+            alert("error getting products")
+        });
+    }
+
+    let tmp = users.map( (u) => {
+        return <p>{u.id}, {u.name}, {u.email} </p>
+    });
+    
     return (
         <div>
             <h3>Rest Calls</h3>
-            <button onClick={getProducsts}>Get products</button>
+            <button onClick={getProducts}>Get products</button>
+            {tmp}
         </div>    
     )
 }
